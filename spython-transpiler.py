984ade1e622295a65bf91063ba92af8e9cd11372
Connor Sun <connorsun@gmail.com>
2024-07-15 20:00:59 -0700

    it's a functional prototype!

--- spython-transpiler.py
+++ spython-transpiler.py
@@ -1,15 +1,26 @@
 from keywords import s_to_py_keywords
 import re
+import tokenize
 
 print("Welcome to the Spython language transpiler!")
 file_path = input("Enter the path to your .spy file: ")
 if (file_path.endswith(".spy")):
     file = open(file_path, "r").read()
-    outfile = file
-    for key in s_to_py_keywords.keys():
-        re.sub(key, s_to_py_keywords[key], outfile)
-        # re.sub(r'[\b$]' + key + r'[\b^]', s_to_py_keywords[key], outfile)
-    print(outfile)
+    outfile = ""
+    tokens = file.split(" ")
+    for token in tokens:
+        new_token = ""
+        for linebroken_token in token.split("\n"):
+            new_token += (s_to_py_keywords[linebroken_token] if linebroken_token in s_to_py_keywords else linebroken_token) + "\n"
+        outfile += " " + new_token[:-1]
+    # print(outfile.strip())
+    # for key in s_to_py_keywords.keys():
+    #     # re.sub(key, s_to_py_keywords[key], outfile)
+    #     re.sub(r'[\b$]' + key + r'[\b^]', s_to_py_keywords[key], outfile)
+    # f = open("file_path"[:-3] + "py", "w")
+    # f.write(outfile.strip())
+    # f.close()
+    exec(outfile.strip())
 else:
     raise Exception("Wrong file extension for input file: " + file_path)
-print(s_to_py_keywords)
+# print(s_to_py_keywords)
